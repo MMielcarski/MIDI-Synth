a175dac65a76455d02853d9640b6c36c46ef33fc
Maciej <mielcarski.m@gmail.com>
2018-11-25 14:21:18 +0100

    all pots working properly without noise, buttons work on rising edge, changed to classic NO pushbuttons, midi ready for polishing

--- v_2/AVR/prototype-2/main.c
+++ v_2/AVR/prototype-2/main.c
@@ -15,11 +15,21 @@
 #define NOTE_ON_CMD 0x90
 #define NOTE_OFF_CMD 0x80
 #define CC_CMD 0xB0
+
 #define PITCH_BEND_CMD 0xE0
+#define MODULATION_WHEEL_CMD 0x01
+#define BREATH_CONTROLLER_CMD 0x02
+#define MAIN_VOLUME_CMD 0x07	
+#define GEN_PURP_CTRL_1_CMD 0x10
+#define GEN_PURP_CTRL_2_CMD 0x11
+#define GEN_PURP_CTRL_3_CMD 0x12
+#define GEN_PURP_CTRL_4_CMD 0x13
+
+#define SOUND_CTRL_6_DECAY_CMD 0x4B
 
 #define VEL_DEF_CMD		0x40	// default 64 velocity
 
-#define CC_VOLUME 0x07	// main volume control (0-127)
+
 
 #define note1 PD0 
 #define note2 PD2 
@@ -40,19 +50,27 @@
 #define octave3 PB6 
 #define octave4 PB7 
 
- int NOTES_tab[13] = {note1,note2,note3,note4,note5,note6,note7,note8,note9,note10,note11,note12,note13};
- int OCTAVES_tab[4] = {octave1,octave2,octave3,octave4};
- int PRESSED_BUTTON_tab[13][4]={0};
+#define button_pin_port PINB 
+#define button1 PB0
+#define button2 PB1
+#define button3 PB2
+#define button4 PB3
 
- int adc_read[8] = {};
- int adc_prev_read[8] = {};
+int NOTES_tab[13] = {note1,note2,note3,note4,note5,note6,note7,note8,note9,note10,note11,note12,note13};
+int OCTAVES_tab[4] = {octave1,octave2,octave3,octave4};
+int PRESSED_KEY_tab[13][4]={0};
 
- int adc_0 = 0, adc_1=0, adc_6=0;
- int adc_0_prev = 0;
-int pitch_bend = 0;
+int BUTTON_tab = {button1, button2, button3, button4};
+int PRESSED_BUTTON_tab[4] = {};
 
-//double control_value = 0;
-//double control_prev_value = 0;
+int adc_read[8] = {};
+int adc_prev_read[8] = {};	
+
+const float EMA_a = 0.6;
+int EMA[8] = {};
+int EMA_prev[8] = {};
+
+int octave_change = 3;
 
 // ------------------------- TIMER ----------------------------------------
 void TIM1_Init(void)	// enable interrupts
@@ -122,94 +140,110 @@ void PORT_Init(void)
 	MCUCSR = (1<<JTD);			//
 	DDRD = 0xFF;
 	DDRC = 0xFF;
+	
+	PORTB |= (1<<button1)|(1<<button2)|(1<<button3)|(1<<button4);	// pull-up on buttons
 }
 
 void note_on(int key, int oct)
 {
 	uart_putchar(NOTE_ON_CMD);		// note on
-	uart_putchar(key + oct * 12);
+	uart_putchar(key + (oct+octave_change) * 12);
 	uart_putchar(VEL_DEF_CMD);
 }
 
 void note_off(int key, int oct)
 {
 	uart_putchar(NOTE_OFF_CMD);		// note off
-	uart_putchar(key + oct * 12);
+	uart_putchar(key + (oct+octave_change) * 12);
 	uart_putchar(VEL_DEF_CMD);
+}
+
+int button_pressed(int pin_port, int pin)
+{
+	if(!(pin_port & (1<<pin)) )
+		return 1;
+	else 
+		return 0;
+}
+
+void CC_send_BUTTON(uint8_t data1, uint8_t data2, int button_pin)
+{
+	if(button_pressed(button_pin_port,button_pin) && !(PRESSED_BUTTON_tab[button_pin]))
+	{
+		PRESSED_BUTTON_tab[button_pin] = 1;
+		uart_putchar(CC_CMD);
+		uart_putchar(data1);		
+		uart_putchar(data2);
+	}
+	else if(!(button_pressed(button_pin_port,button_pin)))
+	{
+		PRESSED_BUTTON_tab[button_pin] = 0;
+	}
 }
 
 void CC_send_ADC(uint8_t control_num,  unsigned int channel)
 {
 	adc_prev_read[channel] = adc_read[channel];
-	adc_read[channel] = ADC_read(channel);
-
-	//value = value*127/1024;
-	//prev_value = prev_value*127/1024;
+	int adc_sum = 0;
+	for(int i=0;i<16;i++)
+	{
+		adc_sum += ADC_read(channel);
+	}
+	adc_read[channel] = adc_sum/16;
 
 	uint8_t adc_7bit = (adc_read[channel] >> 3);
 	uint8_t adc_prev_7bit = (adc_prev_read[channel] >> 3);
 
-	//uart_putint(test1);
-	//uart_putstring("\n");
-
 	if((int)adc_7bit != (int)adc_prev_7bit)
 	{
-		uart_putchar(CC_CMD);
-		uart_putchar(control_num);
-		uart_putchar(adc_7bit);
+		EMA_prev[channel] = EMA[channel];
+		EMA[channel] = (EMA_a*adc_7bit) + (1-EMA_a)*adc_prev_7bit;
+		if(EMA[channel] != EMA_prev[channel])
+		{
+			uart_putchar(CC_CMD);
+			uart_putchar(control_num);
+			uart_putchar(EMA[channel]);
+		}
 	}
 }
 
 ISR(TIMER1_COMPA_vect)	// timer1 overflow interrupt
 {
-		 //adc_0_prev = adc_0;
-	 //adc_0 = ADC_read(0);
+		CC_send_BUTTON(5,6,button1);	// todo: tab with pin numbers
+		CC_send_BUTTON(7,6,button2);
+		CC_send_BUTTON(8,6,button3);
+		CC_send_BUTTON(9,6,button4);
 
-	//if(adc_0 <= (adc_0_prev - 5) || adc_0 >= (adc_0_prev + 5))
-	//{
-		CC_send_ADC(CC_VOLUME, 0);	// slide 1
-		CC_send_ADC(CC_VOLUME, 4);	// knob 3
-		CC_send_ADC(CC_VOLUME, 6);	// joystick 1
-		CC_send_ADC(CC_VOLUME, 7);	// joystick 2
-	//}
-	
-	//uart_putchar(CC_CMD);
-	//uart_putchar(CC_VOLUME);
-	//uart_putchar(adc_0*127/1024);
-	// uart_putchar(PITCH_BEND_CMD);
-	// pitch_bend = 16384 / adc_6;
-	// uart_putchar((unsigned char)(pitch_bend>>7));
-	// uart_putchar((unsigned char)pitch_bend);
-
-	// ------- ADC test -------
-
-	// uart_putstring("\nADC0:");
-	// uart_putint(adc_0);
-	// //uart_putchar('K');
-
-	//adc_1 = ADC_read(1);
-	//uart_putstring("\nADC1:");
-	//uart_putint(adc_1);
-
-	//  adc_6 = ADC_read(6);
-	//  uart_putstring("\nADC6:");
-	//  uart_putint(adc_6);
-}
-
-// ------------------------- MAIN ----------------------------------------
-
-int main(void)
-{
-	USART_Init(MYUBRR);
-	PORT_Init();
-	ADC_Init();
-	TIM1_Init();
-
-	while(1)
+	// octave change:
+	/*if(button_pressed(button_pin_port,button3) && !(PRESSED_BUTTON_tab[button3]))
 	{
-		// ----- keyboard handle -> working without last key --------
+		octave_change++;
+	}
+	else if(!(button_pressed(button_pin_port,button3)))
+	{
+		PRESSED_BUTTON_tab[button3] = 0;
+	}
 
-		for(int i=0; i<13; i++)		// notes loop
+	if(button_pressed(button_pin_port,button4) && !(PRESSED_BUTTON_tab[button4]))
+	{
+		if(octave_change > 0)
+		octave_change--;
+	}
+	else if(!(button_pressed(button_pin_port,button4)))
+	{
+		PRESSED_BUTTON_tab[button4] = 0;
+	}*/
+
+		CC_send_ADC(MAIN_VOLUME_CMD, 0);	// slide 1
+		CC_send_ADC(SOUND_CTRL_6_DECAY_CMD, 1);	// slide 2
+		CC_send_ADC(GEN_PURP_CTRL_1_CMD, 2);	// knob 1 
+		CC_send_ADC(GEN_PURP_CTRL_2_CMD, 3);	// knob 2
+		CC_send_ADC(GEN_PURP_CTRL_3_CMD, 4);	// knob 3
+		CC_send_ADC(GEN_PURP_CTRL_4_CMD, 5);	// knob 4		
+		CC_send_ADC(MODULATION_WHEEL_CMD, 6);	// joystick 1	
+		CC_send_ADC(BREATH_CONTROLLER_CMD, 7);	// joystick 2	
+
+for(int i=0; i<13; i++)		// notes loop
 		{
 			if(i < 7)	// PORTD
 			{
@@ -222,28 +256,28 @@ int main(void)
 
 			// if(i == 12)		// last key case
 			// {
-			// 	if(PINC & (1<<NOTES_tab[12]) && !(PRESSED_BUTTON_tab[12][0]) )
+			// 	if(PINC & (1<<NOTES_tab[12]) && !(PRESSED_KEY_tab[12][0]) )
 			// 	{
-			// 		PRESSED_BUTTON_tab[12][0] = 1;
+			// 		PRESSED_KEY_tab[12][0] = 1;
 			// 		note_on(12,0);
 			// 	}
-			// 	else if( !(PINC & (1<<NOTES_tab[12])) && PRESSED_BUTTON_tab[12][0])
+			// 	else if( !(PINC & (1<<NOTES_tab[12])) && PRESSED_KEY_tab[12][0])
 			// 	{
-			// 		PRESSED_BUTTON_tab[12][0] = 0;
+			// 		PRESSED_KEY_tab[12][0] = 0;
 			// 		note_off(12,0);
 			// 	}
 			// }
 
 			for(int j=0; j<4; j++)		// octaves loop
 			{
-				if( PINB & (1<<OCTAVES_tab[j]) && !(PRESSED_BUTTON_tab[i][j]) )
+				if( PINB & (1<<OCTAVES_tab[j]) && !(PRESSED_KEY_tab[i][j]) )
 				{
-					PRESSED_BUTTON_tab[i][j] = 1;
+					PRESSED_KEY_tab[i][j] = 1;
 					note_on(i,j);
 				}
-				else if( !(PINB & (1<<OCTAVES_tab[j])) && PRESSED_BUTTON_tab[i][j] )
+				else if( !(PINB & (1<<OCTAVES_tab[j])) && PRESSED_KEY_tab[i][j] )
 				{
-					PRESSED_BUTTON_tab[i][j] = 0;
+					PRESSED_KEY_tab[i][j] = 0;
 					note_off(i,j);
 				}
 			}
@@ -257,5 +291,19 @@ int main(void)
 				PORTC &= ~(1<<NOTES_tab[i]);
 			}
 		}
+}
+
+// ------------------------- MAIN ----------------------------------------
+
+int main(void)
+{
+	USART_Init(MYUBRR);
+	PORT_Init();
+	ADC_Init();
+	TIM1_Init();
+
+	while(1)
+	{
+
 	}
 }
